4064cf5cb6541791373334a81650b176c4f6ad74
Adrian Mostowski <akmostowski@gmail.com>
2022-08-13 12:51:06 +0200

    fix e2e test

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -9,6 +9,7 @@ describe("smoke tests", () => {
     const loginForm = {
       email: `${faker.internet.userName()}@example.com`,
       password: faker.internet.password(),
+      name: faker.internet.userName(),
     };
 
     cy.then(() => ({ email: loginForm.email })).as("user");
@@ -18,34 +19,37 @@ describe("smoke tests", () => {
     cy.findByRole("link", { name: /sign up/i }).click();
 
     cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
+    cy.findByLabelText(/name/i).type(loginForm.name);
     cy.findByLabelText(/password/i).type(loginForm.password);
+
     cy.findByRole("button", { name: /create account/i }).click();
 
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByRole("button", { name: /logout/i }).click();
-    cy.findByRole("link", { name: /log in/i });
+    cy.findByText("Book groups");
+    // cy.findByRole("link", { name: /notes/i }).click();
+    // cy.findByRole("button", { name: /logout/i }).click();
+    // cy.findByRole("link", { name: /log in/i });
   });
 
-  it("should allow you to make a note", () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
-    };
-    cy.login();
+  // it("should allow you to make a note", () => {
+  //   const testNote = {
+  //     title: faker.lorem.words(1),
+  //     body: faker.lorem.sentences(1),
+  //   };
+  //   cy.login();
 
-    cy.visitAndCheck("/");
+  //   cy.visitAndCheck("/");
 
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByText("No notes yet");
+  //   cy.findByRole("link", { name: /notes/i }).click();
+  //   cy.findByText("No notes yet");
 
-    cy.findByRole("link", { name: /\+ new note/i }).click();
+  //   cy.findByRole("link", { name: /\+ new note/i }).click();
 
-    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
-    cy.findByRole("button", { name: /save/i }).click();
+  //   cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
+  //   cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
+  //   cy.findByRole("button", { name: /save/i }).click();
 
-    cy.findByRole("button", { name: /delete/i }).click();
+  //   cy.findByRole("button", { name: /delete/i }).click();
 
-    cy.findByText("No notes yet");
-  });
+  //   cy.findByText("No notes yet");
+  // });
 });
