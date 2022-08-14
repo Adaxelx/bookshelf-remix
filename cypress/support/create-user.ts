75be3d57a911899b7d820f9c394684a125ea4cbe
Adrian Mostowski <akmostowski@gmail.com>
2022-08-14 14:13:21 +0200

    fix typescript cypress error

--- cypress/support/create-user.ts
+++ cypress/support/create-user.ts
@@ -19,7 +19,11 @@ async function createAndLogin(email: string) {
     throw new Error("All test emails must end in @example.com");
   }
 
-  const user = await createUser(email, "myreallystrongpassword");
+  const user = await createUser({
+    email,
+    password: "myreallystrongpassword",
+    name: "Random name",
+  });
 
   const response = await createUserSession({
     request: new Request("test://test"),
